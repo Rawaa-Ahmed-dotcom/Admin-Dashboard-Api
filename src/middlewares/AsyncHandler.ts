8db6c447e9214ae1eda4b813ef2c686d22831d26
Rawaa Ahmed <rawaaahmed987@gmail.com>
2026-09-09 22:20:55 +0300

    Fix : implement function with generics to fix issue

--- src/middlewares/AsyncHandler.ts
+++ src/middlewares/AsyncHandler.ts
@@ -1,8 +1,19 @@
 import type {Request , Response , NextFunction, RequestHandler} from "express";
 
 
-export const AsyncHandler =  (fn : RequestHandler) : RequestHandler => {
-    return (req : Request , res : Response , next : NextFunction) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    }
-}
+export const AsyncHandler = <
+ P = {},
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = {}
+>(
+  fn: (
+    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    res: Response,
+    next: NextFunction
+  ) => Promise<any>
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
